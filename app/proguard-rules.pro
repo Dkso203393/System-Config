972ba09806aa0147ca81038a9d5625181c7ccf79
Dkso203393 <adityajjadhav1@gmail.com>
2025-05-27 00:48:35 +0530

    Initial commit

---
+++ app/proguard-rules.pro
@@ -0,0 +1,3 @@
+# Keep classes for Google Mobile Ads SDK
+-keep class com.google.android.gms.ads.** { *; }
+-dontwarn com.google.android.gms.ads.**
---
+++ app/src/main/java/com/example/workingadmobtestapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.workingadmobtestapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+    private AdView adView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, initializationStatus -> {});
+        adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+    }
+}
